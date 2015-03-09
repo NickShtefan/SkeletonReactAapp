e4ef3b5829be31872d0e48f389c17745c8c789c1
shtefan <2>
2015-03-10 00:29:00 +0300

    /

--- source/serverApp.js
+++ source/serverApp.js
@@ -7,8 +7,6 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var React = require('react');
-var routes = require('./routes/index');
-var users = require('./routes/users');
 
 var Dispatcher = require( './dispatcher/Dispatcher');
 var ActionTypes = require( './constants/ActionTypes');
